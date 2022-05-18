e02b356dbc983cccb829683e4a1b57ec5f24c377
Carl Abel <caweb.cph@gmail.com>
2022-05-18 21:19:29 +0200

    TravelComponent update

--- src/TravelComponent.jsx
+++ src/TravelComponent.jsx
@@ -1,24 +1,48 @@
 /** @jsxImportSource @emotion/react */
-import { css } from "@emotion/react"
+import { css } from "@emotion/react";
 
-const TravelComponent = ({ sections, title }) => {
+const TravelComponent = (props) => {
   const styles = {
-    h1: css`
-      padding: 10px;
+    article: css`
+      margin: 30px;
+      padding: 30px;
+      width: 33%;
+      height: 500px;
+
+      border: solid 1px red;
+      border-radius: 24px;
+
+      & span {
+        text-transform: capitalize;
+        font-size: 26px;
+        font-weight: bold;
+        color: darkblue;
+      }
+      & h2 {
+        font-size: 32px;
+      }
+      & p {
+        font-size: 1.5em;
+        font-weight: 500;
+        color: #3a3a3a;
+      }
     `,
-  }
+  };
+
+  const data = props.data;
 
   return (
-    <div className="TravelComponent">
-      <span>{}</span>
-      {sections.map((section) => (
-        <div key={section}>
-          <h2 css={styles.h1}>{section.header.title}</h2>
-          <p>{}</p>
-        </div>
+    <section>
+      {data.map((section) => (
+        <article css={styles.article} key={section.id}>
+          <img src="#" alt="#" />
+          <span>{section.tag}</span>
+          <h2>{section.title}</h2>
+          <p>{section.body}</p>
+        </article>
       ))}
-    </div>
-  )
-}
+    </section>
+  );
+};
 
-export default TravelComponent
+export default TravelComponent;
